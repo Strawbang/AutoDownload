6d1e1554ffd03d53748b3c7b076631145d151c7b
Strawbang <dbougouffa@gmail.com>
2017-03-30 15:06:09 +0200

    update

--- gestiontorrent.py
+++ gestiontorrent.py
@@ -6,9 +6,10 @@ import datetime
 reload(sys) # Reload does the trick!
 sys.setdefaultencoding('UTF8')
 #On part du principe que vous utilisez les ports de base pour votre transmission et on utilise le port habituel 9091.
+tc = transmissionrpc.Client('localhost', port=9091, user='admin', password='')
 
 #On récupére la date actuelle
-date = datetime.datetime.now()
+datenow = datetime.datetime.now()
 today2heure = now.replace(hour=2, minute=0, second=0, microsecond=0)
 today7heure = now.replace(hour=7, minute=0, second=0, microsecond=0)
 #On affiche tous les torrents en cours.
@@ -16,7 +17,7 @@ print tc.get_torrents()
 print 'il y a: ',len(tc.get_torrents()),' torrents dans la liste'
 #Pour chaque torrent en cours on va faire une serie de vérifications.
 for torrent in tc.get_torrents() :
-	if date > today2heure and date < today7heure :
+	if datenow > today2heure :
 		tc.start_torrent(torrent.id,True)
-	if date > today7heure and date < today2heure :
+	if datenow > today7heure :
 		tc.stop_torrent(torrent.id,True)
